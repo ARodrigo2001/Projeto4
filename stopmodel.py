63cb64c9f6c0317435f4c57afc6be3c2de54a429
Arthur Rodrigo <arthurelio_tuca12@hotmail.com>
2021-11-26 18:30:38 -0300

    Add files via upload

---
+++ stopmodel.py
@@ -0,0 +1,30 @@
+#Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#PDX-License-Identifier: MIT-0 (For details, see https://github.com/awsdocs/amazon-rekognition-custom-labels-developer-guide/blob/master/LICENSE-SAMPLECODE.)
+
+import boto3
+import time
+
+
+def stop_model(model_arn):
+
+    client=boto3.client('rekognition')
+
+    print('Stopping model:' + model_arn)
+
+    #Stop the model
+    try:
+        response=client.stop_project_version(ProjectVersionArn=model_arn)
+        status=response['Status']
+        print ('Status: ' + status)
+    except Exception as e:  
+        print(e)  
+
+    print('Done...')
+    
+def main():
+    
+    model_arn='arn:aws:rekognition:us-east-1:017259909827:project/flowers-custom-labels/version/flowers-custom-labels.2021-11-25T21.41.24/1637887284777'
+    stop_model(model_arn)
+
+if __name__ == "__main__":
+    main() 
